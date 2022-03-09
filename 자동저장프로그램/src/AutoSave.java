ab7c3073e31296476b0097198bdc41e1afc871a4
songhyun lee <myy32612@gmail.com>
2022-03-09 18:51:10 +0900

    기존 저장 내용 가져오기 + 자동 저장 시간 확인

--- 자동저장프로그램/src/AutoSave.java
+++ 자동저장프로그램/src/AutoSave.java
@@ -4,12 +4,15 @@ import javax.swing.Timer;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -17,6 +20,8 @@ import java.awt.SystemColor;
 import java.awt.Color;
 
 public class AutoSave extends JFrame {
+	File myDiary = new File("C:\\오늘의 일기\\" + String.valueOf(LocalDate.now()) + ".txt");
+	BufferedReader br;
 
 	public AutoSave() {
 		super("오늘의 일기");
@@ -29,6 +34,25 @@ public class AutoSave extends JFrame {
 
 		JTextArea contents = new JTextArea();
 		contents.setLineWrap(true);
+		try {
+			br = new BufferedReader(new FileReader(myDiary));
+		} catch (FileNotFoundException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		if (myDiary.exists()) {
+			String line;
+			StringBuilder sb = new StringBuilder();
+			try {
+				while ((line = br.readLine()) != null) {
+					sb.append(line);
+					contents.setText(sb.toString());
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
 
 		contents.setFont(new Font("HY신명조", Font.PLAIN, 20));
 		contents.setBackground(SystemColor.inactiveCaptionBorder);
@@ -43,7 +67,7 @@ public class AutoSave extends JFrame {
 
 		JLabel info = new JLabel();
 		info.setFont(new Font("HY신명조", Font.ITALIC, 16));
-		info.setBounds(281, 414, 176, 27);
+		info.setBounds(14, 414, 443, 27);
 		getContentPane().add(info);
 
 		Timer timer = new Timer(10000, new ActionListener() {
@@ -53,8 +77,6 @@ public class AutoSave extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				File directory = new File("C:\\오늘의 일기");
 				directory.mkdir();
-
-				File myDiary = new File("C:\\오늘의 일기\\" + String.valueOf(LocalDate.now()) + ".txt");
 
 				if (!myDiary.exists()) {
 					try {
@@ -67,15 +89,20 @@ public class AutoSave extends JFrame {
 				}
 
 				try {
+
 					bw = new BufferedWriter(new FileWriter(myDiary));
 				} catch (IOException e1) {
 
 					e1.printStackTrace();
 				}
 				try {
-					bw.write(contents.getText());
 
-					info.setText("임시 저장 되었습니다");
+					bw.write(contents.getText());
+					String hour = String.valueOf(LocalTime.now().getHour());
+					String min = String.valueOf(LocalTime.now().getMinute());
+					String sec = String.valueOf(LocalTime.now().getSecond());
+
+					info.setText("임시 저장 되었습니다     " + LocalDate.now() + "  " + hour + "시 " + min + "분 " + sec + "초 ");
 				} catch (IOException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
